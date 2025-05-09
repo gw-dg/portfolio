c234823c79af7ad33b1e3aafd6be6fc17918b92c
gw-dg <92924837+gw-dg@users.noreply.github.com>
2025-05-09 15:00:07 +0530

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider defaultTheme="dark">{children}</ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
