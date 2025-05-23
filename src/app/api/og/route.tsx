24c3273aea73ff838d98d4bda0fd4823cf5ca4f9
gw-dg <92924837+gw-dg@users.noreply.github.com>
2025-05-23 21:15:20 +0530

    any

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -191,8 +191,8 @@ export async function GET(request: NextRequest) {
         height: 630,
       }
     );
-  } catch (e: any) {
-    console.log(`${e.message}`);
+  } catch (e: unknown) {
+    console.log(`${e instanceof Error ? e.message : "An error occurred"}`);
     return new Response(`Failed to generate the image`, {
       status: 500,
     });
